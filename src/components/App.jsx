cc3ce05487e45b858556ce3ad202b2529349050d
Alexandre <alexandrefsoficial@gmail.com>
2023-01-17 16:15:43 -0300

    login route implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import GlobalStyle from '../styles/globalStyles';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Login from '../pages/Login'
+import Login from '../pages/Login';
 
 export default function App() {
   return (
@@ -10,7 +10,7 @@ export default function App() {
       <GlobalStyle/>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login/>}/>
+          <Route path="/" element={<Login />}/>
           {/*
           <Route path="/sessoes/:idFilme" element={<SecondPage time={timeTable} setTime={setTimeTable}/>}/>
           <Route path="/assentos/:idSessao" element={<ThirdPage ticket={ticketDetails} setTicket={setTicketDetails}/>}/>
